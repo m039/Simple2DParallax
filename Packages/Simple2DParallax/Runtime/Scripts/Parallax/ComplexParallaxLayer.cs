c556b51835d3322218e49c3a51589a9e6d4b709a
Dmitry Mozgin <m0391n@gmail.com>
2019-12-31 19:23:32 +0300

    Added a link to CommonUnityLibrary

---
+++ Packages/Simple2DParallax/Runtime/Scripts/Parallax/ComplexParallaxLayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace m039.Parallax
+{
+
+	public class ComplexParallaxLayer : BaseParallaxLayer
+	{
+
+		#region Inspector
+
+		[Header("Complex Parallax Settings")]
+		[Range(0f, 360f)]
+		public float angle;
+
+		public float speed;
+
+		#endregion
+
+
+	}
+
+}
