49eb88a26245815be41e224c8dbf4623f9663227
Dmitry Mozgin <m0391n@gmail.com>
2020-01-01 00:15:53 +0300

    Added a filled with squares layer.

---
+++ Assets/Simple2DParallax/Scripts/Example/PrimitivesParallaxExample.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace m039.Parallax
+{
+
+	public class PrimitivesParallaxExample : MonoBehaviour
+	{
+
+
+	}
+
+}
